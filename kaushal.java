c5714d9b568c72ccc6cc330d0e11757e6b739f10
Kaushal-Devcode <kaushaldevgaonkar03@gmail.com>
2026-03-01 12:16:58 +0530

    practice java

---
+++ kaushal.java
@@ -0,0 +1,15 @@
+class Solution{
+    String phone_name = "samsung";
+    int price = 200;
+
+    void print(){
+        System.out.println("brand name:" + phone_name + "  device_price :" + price);
+    }
+}
+public class kaushal {
+    public static void main(String[] args) {
+         Solution sol = new Solution();
+        sol.print();
+        System.out.println("hello world");
+    }
+}
